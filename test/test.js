6c13ffbef09f76eeecefa98b1dded4bee338106e
Daniel Ribeiro <danrbr@gmail.com>
2013-09-24 23:54:55 -0700

    dummy test passing

---
+++ test/test.js
@@ -0,0 +1,12 @@
+/**
+ * @author bhouston / http://exocortex.com
+ */
+
+module( "TestDummy" );
+
+test( "test", function() {
+    var a = new THREE.Box2();
+    ok( 1 ==  1, "Passed!" );
+    
+});
+
