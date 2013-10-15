458c7a1a4b8c7b105162b672f7834443dda47154
Daniel Ribeiro <danrbr@gmail.com>
2013-10-14 22:34:25 -0700

    adding a failing test

--- test/test.js
+++ test/test.js
@@ -1,13 +1,25 @@
 module( "Mirror Test" );
 
 asyncTest( "mirror demo ", function() {
-    mirror_sample("mirror-test");
-    var canvas = document.getElementById("mirror-test");
+    var canvas = "mirror-test"
+    mirror_sample(canvas);
+    var canvas = document.getElementById(canvas);
     canvas.setAttribute("class", "input-canvas")
-    // canvas.width = 1400;
-    // canvas.height = 800;
     var result = canvas.toDataURL("image/png");
-    // window.open(result)
+    resemble("fixtures/mirror.png").compareTo(result).onComplete(function(result) {
+        console.log(result)
+        equal(true, result.isSameDimensions, "Images have the same dimensions");
+        equal(0, result.misMatchPercentage, "Images are the same");
+        start();
+    })
+});
+
+asyncTest( "mirror demo needs at least two iteratino counts", function() {
+    var canvas = "mirror-test-fail"
+    mirror_sample(canvas, 1);
+    var canvas = document.getElementById(canvas);
+    canvas.setAttribute("class", "input-canvas")
+    var result = canvas.toDataURL("image/png");
     resemble("fixtures/mirror.png").compareTo(result).onComplete(function(result) {
         console.log(result)
         equal(true, result.isSameDimensions, "Images have the same dimensions");
