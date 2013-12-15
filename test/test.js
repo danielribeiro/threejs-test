09325b0ad275332c139d371e4a34120cd5e235e3
Daniel Ribeiro <danrbr@gmail.com>
2013-12-14 23:29:00 -0800

    sceneTest complexity externalized to a helper function

--- test/test.js
+++ test/test.js
@@ -13,63 +13,36 @@ function sceneTest(name, block) {
   asyncTest (name, function() {
     var canvasName = name.replace (/\s+/g, "-");
     Math.reseed ();
-    var resembleFn = function() {
+    var resembleFn = function(fixtureName) {
       var canvas = document.getElementById (canvasName);
       canvas.setAttribute ("class", "input-canvas")
       var result = canvas.toDataURL ("image/png");
-      resemble ("fixtures/collada.png").compareTo (result).onComplete (function(result) {
+      resemble ("fixtures/" + fixtureName + ".png").compareTo (result).onComplete (function(result) {
         displayDiffImage (result);
         equal (true, result.isSameDimensions, "Images have the same dimensions");
         equal (0, result.misMatchPercentage, "Images are the same");
         start ();
       });
     }
-    block (canvasName, resembleFn);
+    block(canvasName, resembleFn);
   });
 };
 
-sceneTest ("mirror demo ", function(canvasName) {
+sceneTest ("mirror demo ", function(canvasName, resembleFn) {
   mirror_sample (canvasName);
-  var canvas = document.getElementById (canvasName);
-  canvas.setAttribute ("class", "input-canvas")
-  var result = canvas.toDataURL ("image/png");
-  resemble ("fixtures/mirror.png").compareTo (result).onComplete (function(result) {
-    displayDiffImage (result);
-    equal (true, result.isSameDimensions, "Images have the same dimensions");
-    equal (0, result.misMatchPercentage, "Images are the same");
-    start ();
-  })
+  resembleFn("mirror")
 });
 
-sceneTest ("mirror demo needs at least two iteration counts", function(canvasName) {
+sceneTest ("mirror demo needs at least two iteration counts", function(canvasName, resembleFn) {
   mirror_sample (canvasName, 1);
-  var canvas = document.getElementById (canvasName);
-  canvas.setAttribute ("class", "input-canvas")
-  var result = canvas.toDataURL ("image/png");
-  resemble ("fixtures/mirror.png").compareTo (result).onComplete (function(result) {
-    displayDiffImage (result);
-    equal (true, result.isSameDimensions, "Images have the same dimensions");
-    equal (0, result.misMatchPercentage, "Images are the same");
-    start ();
-  })
+  resembleFn("mirror")
 });
 
 sceneTest ("Collada works", function(canvasName, resembleFn) {
-  collada_sample (canvasName, 30, resembleFn);
+  collada_sample (canvasName, 30, function() { resembleFn("collada") });
 });
 
 
-sceneTest ("Collada fails with a different morph target", function(canvasName) {
-  collada_sample (canvasName, 29,
-    function() {
-      var canvas = document.getElementById (canvasName);
-      canvas.setAttribute ("class", "input-canvas")
-      var result = canvas.toDataURL ("image/png");
-      resemble ("fixtures/collada.png").compareTo (result).onComplete (function(result) {
-        displayDiffImage (result);
-        equal (true, result.isSameDimensions, "Images have the same dimensions");
-        equal (0, result.misMatchPercentage, "Images are the same");
-        start ();
-      })
-    });
+sceneTest ("Collada fails with a different morph target", function(canvasName, resembleFn) {
+  collada_sample (canvasName, 29, function() { resembleFn("collada") });
 });
