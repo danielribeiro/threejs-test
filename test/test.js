a2de7c2ce80756a890687947eb4166d205dda2bf
Daniel Ribeiro <danrbr@gmail.com>
2014-02-08 17:56:47 -0800

    loading image so that we can setup auto diff in case it fails

--- test/test.js
+++ test/test.js
@@ -6,26 +6,37 @@ function displayDiffImage(result) {
   diffImage.src = result.getImageDataUrl();
   var imageDiffs = document.getElementById(QUnit.config.current.id);
   imageDiffs.appendChild(diffImage);
+}
 
+function withImage(url, callback) {
+  var img = new Image();
+  img.onload = function() {
+    callback(img)
+  };
+  img.src = url;
 }
 
 function sceneTest(name, block) {
-  asyncTest(name, function() {
+  asyncTest(name, function () {
     var canvasName = name.replace(/\s+/g, "-");
-    var resembleFn = function(fixtureName, canvas) {
+    var resembleFn = function (fixtureName, canvas) {
       canvas.setAttribute("id", canvasName);
       canvas.setAttribute("class", "input-canvas");
       var result = canvas.toDataURL("image/png");
-      resemble("fixtures/" + fixtureName + ".png").compareTo(result).onComplete(function(result) {
-        displayDiffImage(result);
-        equal(true, result.isSameDimensions, "Images have the same dimensions");
-        equal(0, result.misMatchPercentage, "Images are the same");
-        start();
+      var url = "fixtures/" + fixtureName + ".png"
+      withImage(url, function (img) {
+        resemble(url).compareTo(result).onComplete(function (result) {
+          displayDiffImage(result);
+          equal(true, result.isSameDimensions, "Images have the same dimensions");
+          equal(0, result.misMatchPercentage, "Images are the same");
+          start();
+        });
       });
+
     }
     block(resembleFn);
   });
-};
+}
 
 sceneTest("mirror demo ", function(resembleFn) {
   resembleFn("mirror", mirror_sample());
